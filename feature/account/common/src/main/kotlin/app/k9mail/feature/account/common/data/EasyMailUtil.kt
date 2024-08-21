9c6a8dc4ad540244210b65b79137e818869bd0e8
hungnguyen6 <hungnguyen6@tdt.asia>
2024-08-21 13:38:11 +0700

    get saved account from EasyMail - fake data

--- feature/account/common/src/main/kotlin/app/k9mail/feature/account/common/data/EasyMailUtil.kt
+++ feature/account/common/src/main/kotlin/app/k9mail/feature/account/common/data/EasyMailUtil.kt
@@ -7,12 +7,33 @@ import com.hungbang.email2018.data.entity.SignInConfigs
 import io.paperdb.Paper
 
 object EasyMailUtil {
+    /**
+     * used to get account email & password
+     */
     fun getSavedAccountFromEasyMail(): Account? {
-        return Paper.book().read<Account>("CURRENT_ACCOUNT", null)
+        val fakeAcc = Account().apply {
+            accountEmail = "easyai.group@mailo.com"
+            password = "Matkhausieumanh1"
+        }
+//        return Paper.book().read<Account>("CURRENT_ACCOUNT", null)
+        return fakeAcc
     }
 
+    /**
+     * used to get host, port...
+     */
     fun getSavedSignInConfigFromEasyMail(mailDomain: String): SignInConfigs? {
-        return Paper.book().read<SignInConfigs>("KEY_CONFIG_SIGNIN$mailDomain", null)
+        val fakeConfigs = SignInConfigs(
+            mailDomain,
+            "mail.mailo.com",
+            "993",
+            "1",
+            "mail.mailo.com",
+            "465",
+            "1"
+        )
+        return fakeConfigs
+//        return Paper.book().read<SignInConfigs>("KEY_CONFIG_SIGNIN$mailDomain", null)
     }
 
     fun testGetSavedDataFromEasyMail(){
