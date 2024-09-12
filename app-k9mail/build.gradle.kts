d3f2777d84b460243a5db615bd6f9920865cb5a7
NamTran <trandinhnam1199@gmail.com>
2024-09-12 17:05:01 +0700

    config build release

--- app-k9mail/build.gradle.kts
+++ app-k9mail/build.gradle.kts
@@ -47,7 +47,7 @@ android {
 //        applicationId = "com.hungbang.email2018"
         testApplicationId = "email.swift.ai.smart.allmail"
 
-        versionCode = 109
+        versionCode = 200
         versionName = "6.905-SNAPSHOT"
 
         // Keep in sync with the resource string array "supported_languages"
