f71a760a845e88c5392cddc5a2002816c562fce7
NamTran <trandinhnam1199@gmail.com>
2024-09-14 22:29:28 +0700

    config bug crash when update

--- build-plugin/src/main/kotlin/thunderbird.app.android.compose.gradle.kts
+++ build-plugin/src/main/kotlin/thunderbird.app.android.compose.gradle.kts
@@ -12,8 +12,9 @@ android {
     }
 
     buildTypes {
-        debug {
-            isMinifyEnabled = false
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
